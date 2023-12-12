0dc0373eaa352465bd41c53544cad1167b7df249
JanSwiatek1 <122283484+JanSwiatek1@users.noreply.github.com>
2023-12-12 19:58:45 +0000

    Deploying to gh-pages from @ JanSwiatek1/goit-js-hw-09@157372468e22833082cf3633bae783b978385f32 🚀

--- 01-color-switcher.a2656dcb.js
+++
@@ -1,2 +0,0 @@
-!function(){var t,e=document.querySelector("[data-start]"),r=document.querySelector("[data-stop]");e.addEventListener("click",(function(){e.hasAttribute("disabled")||(t=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),r.removeAttribute("disabled"),e.setAttribute("disabled",!0))})),r.addEventListener("click",(function(){clearInterval(t),r.removeAttribute("disabled",!0),e.toggleAttribute("disabled")}))}();
-//# sourceMappingURL=01-color-switcher.a2656dcb.js.map
